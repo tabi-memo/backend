619799e8ae1ccfbe93c747e40ac8f5ec4c95edb8
Kana Taguchi <mukrabwy103@gmail.com>
2023-12-22 16:53:31 -0800

    Change schema time_from and date_from for activity and trip

---
+++ supabase/migrations/20231223004308_amend_activity_time_from_and_trip_time_from.sql
@@ -0,0 +1,5 @@
+-- Change time_from to NOT NULL on activity table
+ALTER TABLE activity ALTER COLUMN time_from SET NOT NULL;
+
+-- Change time_to to NOT NULL on trip table
+ALTER TABLE trips ALTER COLUMN date_from SET NOT NULL;
