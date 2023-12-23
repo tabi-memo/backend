1892a9e2fe6ed2502a337be7003c8eb2052cd26d
Kana Taguchi <mukrabwy103@gmail.com>
2023-12-22 16:56:20 -0800

    Fix text

--- supabase/migrations/20231223004308_amend_activity_time_from_and_trip_time_from.sql
+++ supabase/migrations/20231223004308_amend_activity_time_from_and_trip_time_from.sql
@@ -1,5 +1,5 @@
 -- Change time_from to NOT NULL on activity table
 ALTER TABLE activity ALTER COLUMN time_from SET NOT NULL;
 
--- Change time_to to NOT NULL on trip table
+-- Change date_from to NOT NULL on trip table
 ALTER TABLE trips ALTER COLUMN date_from SET NOT NULL;
