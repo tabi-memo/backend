6d694d68b7c2f5a1097e6a4ab580aa870f30f699
Kana Taguchi <mukrabwy103@gmail.com>
2023-12-22 22:03:11 -0800

    Change migration

--- supabase/migrations/20231223004308_amend_activity_time_from_and_trip_time_from.sql
+++
@@ -1,5 +0,0 @@
--- Change time_from to NOT NULL on activity table
-ALTER TABLE activity ALTER COLUMN time_from SET NOT NULL;
-
--- Change date_from to NOT NULL on trip table
-ALTER TABLE trips ALTER COLUMN date_from SET NOT NULL;
